e20a6e4a9a8592001284dae7bba50aa85fd3179b
BsImtiaz <bsimtiaz004@gmail.com>
2026-05-17 13:40:18 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,60 +1,36 @@
 const express = require('express');
+const axios = require('axios');
+
 const public_users = express.Router();
 
 let books = require("./booksdb.js");
 
-public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books,null,4));
+public_users.get('/', async function (req, res) {
+    const response = await axios.get('http://localhost:5000/');
+    res.send(response.data);
 });
 
-public_users.get('/isbn/:isbn',function (req, res) {
-
-  const isbn = req.params.isbn;
-
-  res.send(books[isbn]);
-
+public_users.get('/isbn/:isbn', async function (req, res) {
+    const isbn = req.params.isbn;
+    const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+    res.send(response.data);
 });
 
-public_users.get('/author/:author',function (req, res) {
-
-  const author = req.params.author;
-  let filtered_books = {};
-
-  Object.keys(books).forEach(key => {
-
-    if(books[key].author === author){
-      filtered_books[key] = books[key];
-    }
-
-  });
-
-  res.send(filtered_books);
-
+public_users.get('/author/:author', async function (req, res) {
+    const author = req.params.author;
+    const response = await axios.get(`http://localhost:5000/author/${author}`);
+    res.send(response.data);
 });
 
-public_users.get('/title/:title',function (req, res) {
-
-  const title = req.params.title;
-  let filtered_books = {};
-
-  Object.keys(books).forEach(key => {
-
-    if(books[key].title === title){
-      filtered_books[key] = books[key];
-    }
-
-  });
-
-  res.send(filtered_books);
-
+public_users.get('/title/:title', async function (req, res) {
+    const title = req.params.title;
+    const response = await axios.get(`http://localhost:5000/title/${title}`);
+    res.send(response.data);
 });
 
-public_users.get('/review/:isbn',function (req, res) {
-
-  const isbn = req.params.isbn;
-
-  res.send(books[isbn].reviews);
-
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
